32cf9828b9594a4da1c48f3264b37391f6296a70
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-04 23:48:33 -0800

    add wallet01 and tailwindcss

---
+++ bookmark-app/components/WalletButtons.tsx
@@ -0,0 +1,42 @@
+import { useClient, useConnect, useWallet } from "@wallet01/react";
+import { toast } from "react-hot-toast";
+
+const ConnectButtons = () => {
+  const { connectors, isAutoConnecting } = useClient();
+  const { activeConnector, isConnected, did, address } = useWallet();
+  const { connect, isError, error } = useConnect();
+
+  if (isError && error) {
+    toast.error(error.message);
+  }
+
+  if (isAutoConnecting) {
+    console.log("isAutoConnecting");
+  }
+
+  return (
+    <div className="flex flex-col">
+      <span className="font-bold text-lg text-center w-full">
+        Choose the desired Wallet
+      </span>
+
+      <div className="grid grid-cols-3 gap-4 items-center mt-5 w-fit justify-center">
+        {connectors.map((connector) => (
+          <button
+            key={connector.name}
+            disabled={isConnected && connector !== activeConnector}
+            onClick={() => {
+              connect({
+                connector: connector,
+              });
+              toast(connector.name);
+            }}
+            className="w-32 h-32 p-3 text-lg flex justify-center items-center leading-relaxed aspect-square w-full rounded-lg border border-slate-600 font-bold bg-slate-700"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ConnectButtons;
