98c4e465634f278976f2a5609ace613ae6e0100f
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-08 15:35:16 -0800

    add nft port api

--- bookmark-app/pages/match.tsx
+++ bookmark-app/pages/match.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import VideoCall from "../components/match/VideoCall";
+import axios from 'axios';
 
 export default function Match() {
+
+  const fetchNftsOwned = () => axios.get('/api/nftsowned');
+
+  console.log('fetchNftsOwned', fetchNftsOwned);
+
   return (
     <div className="flex h-screen w-screen">
         <VideoCall />
