56ca5499bc8336827b5f3934519bac4da7a5cc6f
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-08 18:49:05 -0800

    chore: add nft port api

--- bookmark-app/pages/match.tsx
+++ bookmark-app/pages/match.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import VideoCall from "../components/match/VideoCall";
-import axios from 'axios';
+import { useWallet } from "@wallet01/react";
+import { reduceEachTrailingCommentRange } from "typescript";
 
 export default function Match() {
+  const [nftsOwned, setNftsOwned] = useState<any>();
+  const { address: userAddress } = useWallet();
 
-  const fetchNftsOwned = () => axios.get('/api/nftsowned');
+  useEffect(() => {
+    if (userAddress) {
+      fetch(`/api/${userAddress}`)
+        .then((response) => response.json())
+        .then((response) => setNftsOwned(response));
+    }
+  }, [userAddress]);
 
-  console.log('fetchNftsOwned', fetchNftsOwned);
+  const getContractOwners = useMemo(() => {
+    if (!nftsOwned || !userAddress) return;
+    const nftsOwnedArray = nftsOwned?.nfts?.map((value: { creator_address: string }) => {
+        return value.creator_address;
+      });
+    return nftsOwnedArray.filter(function(item: string) {
+        return item !== userAddress.toString();
+    })
+  }, [nftsOwned]);
+
+  const randomNumber = Math.floor(Math.random() * getContractOwners?.length);
+  const randomAddress = getContractOwners?.[randomNumber];
 
   return (
     <div className="flex h-screen w-screen">
-        <VideoCall />
+      <VideoCall walletAddress={randomAddress} />
     </div>
   );
 }
