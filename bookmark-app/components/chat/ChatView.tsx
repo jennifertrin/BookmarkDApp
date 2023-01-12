4cf9072a025ae18bf8d1c47e80798ac945e85098
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-11 18:13:40 -0800

    add xmtp signing and sending

--- bookmark-app/components/chat/ChatView.tsx
+++ bookmark-app/components/chat/ChatView.tsx
@@ -6,26 +6,100 @@ import {
   Message,
   MessageInput,
 } from "@chatscope/chat-ui-kit-react";
+import { Client } from "@xmtp/xmtp-js";
+import { useCallback, useMemo, useState } from "react";
+import { ethers } from "ethers";
+import { useWallet } from "@wallet01/react";
 
-export default function ChatView() {
+type ChatViewProps = {
+  walletAddress: string;
+};
+
+export default function ChatView({ walletAddress }: ChatViewProps) {
+  const [isXmtpClient, setIsXmtpClient] = useState<boolean>(false);
+  const [isNotOnNetwork, setIsNotOnNetwork] = useState<boolean>(false);
+  const { address: userAddress } = useWallet();
+
+  const provider: ethers.providers.Web3Provider | undefined = useMemo(() => {
+    if (window?.ethereum) {
+      return new ethers.providers.Web3Provider(window.ethereum);
+    }
+  }, []);
+
+  const signer: any = useMemo(() => {
+    if (!userAddress || !provider) return;
+    return provider.getSigner();
+  }, [provider, userAddress]);
+
+  const conversation = useCallback(async () => {
+    if (!walletAddress) return;
+    try {
+      const xmtp = await Client.create(signer);
+      if (xmtp) {
+        setIsXmtpClient(true);
+      }
+      const conversation = await xmtp.conversations.newConversation(
+        walletAddress
+      );
+      return conversation;
+    } catch (e: any) {
+      setIsXmtpClient(false);
+      console.log(e);
+      if (e.message.includes("not on the XMTP network")) {
+        setIsNotOnNetwork(true);
+      }
+    }
+  }, [signer, walletAddress]);
+
+  const messages = useCallback(async () => {
+    const newConversations = await conversation();
+    if (!newConversations) return;
+    const messages = await newConversations.messages().then(response => { response });
+    return messages;
+  }, []);
+
+  async function sendMessage(textContent: string) {
+    const newConversations = await conversation();
+    if (!newConversations) return;
+    const sendMessage = newConversations.send(textContent);
+    return sendMessage;
+  }
+
   return (
-    <div style={{ position: "relative", height: "500px" }}>
-      <MainContainer>
-        <ChatContainer>
-          <MessageList>
-            <Message
-              model={{
-                message: "Hello my friend",
-                sentTime: "just now",
-                sender: "Joe",
-                direction: "incoming",
-                position: "first",
-              }}
-            />
-          </MessageList>
-          <MessageInput placeholder="Type message here" />
-        </ChatContainer>
-      </MainContainer>
+    <div>
+      {!isXmtpClient && (
+        !isNotOnNetwork ? 
+        <button
+          onClick={() => conversation()}
+          className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        > Start Conversation with {walletAddress}
+        </button> : 
+        <div
+          className="flex px-4 py-2 w-24"
+        > {walletAddress} is not on the network. You cannot chat with them via chat at the moment
+        </div>)}
+        {isXmtpClient && <div
+          style={{
+            position: "relative",
+            height: "700px",
+            marginLeft: "5%",
+            width: "100%",
+          }}
+        >
+          <MainContainer>
+            <ChatContainer>
+              <MessageList>
+              </MessageList>
+              <MessageInput
+                placeholder="Type message here"
+                sendButton={true}
+                onSend={(textContent) => {
+                  sendMessage(textContent);
+                }}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </div>}
     </div>
   );
 }
--- bookmark-app/components/match/VideoCall.tsx
+++ bookmark-app/components/match/VideoCall.tsx
@@ -8,16 +8,18 @@ type VideoCallProps = {
 export default function VideoCall({walletAddress} : VideoCallProps) {
   const iframeConfig: IframeConfig = {
     roomUrl: "https://iframe.huddle01.com/123",
-    height: "600px",
-    width: "100%",
+    height: "800px",
+    width: "80%",
     noBorder: true, 
   };
 
   return (
     <div className="flex flex-col mx-auto h-screen w-screen">
       <div className="text-xl my-5 mx-auto">You&apos;ve been matched with: {walletAddress}</div>
+      <div className="flex flex-row">
       <HuddleIframe config={iframeConfig} />
-      <ChatView />
+      <ChatView walletAddress={walletAddress} />
+      </div>
     </div>
   );
 }
